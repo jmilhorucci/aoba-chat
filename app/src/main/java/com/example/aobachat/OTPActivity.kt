a72925d259516eb44437b8563934150fdf5319df
jmilhorucci <juliano.victor98@hotmail.com>
2022-04-28 03:58:00 -0300

    telas celular e validação OTP criadas.

--- app/src/main/java/com/example/aobachat/OTPActivity.kt
+++ app/src/main/java/com/example/aobachat/OTPActivity.kt
@@ -1,11 +1,81 @@
 package com.example.aobachat
 
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.example.aobachat.databinding.ActivityOtpactivityBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class OTPActivity : AppCompatActivity() {
+
+    var binding : ActivityOtpactivityBinding? = null
+    var verificationId : String? = null
+    var auth : FirebaseAuth? = null
+    var dialog : ProgressDialog? = null
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otpactivity)
+        binding = ActivityOtpactivityBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog = ProgressDialog(this@OTPActivity)
+        dialog!!.setMessage("Enviando código OTP...")
+        dialog!!.setCancelable(false)
+        dialog!!.show()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        val phoneNumber = intent.getStringExtra("phoneNumber")
+        binding!!.lblePhone.text = "Verifique $phoneNumber"
+
+        val options = PhoneAuthOptions.newBuilder(auth!!)
+            .setPhoneNumber(phoneNumber!!)
+            .setTimeout(60L,TimeUnit.SECONDS)
+            .setActivity(this@OTPActivity)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCodeSent(
+                    verifyId: String,
+                    forceResendingToken: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(verifyId, forceResendingToken)
+                    dialog!!.dismiss()
+                    verificationId = verifyId
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+                    binding!!.otpView.requestFocus()
+
+                }
+
+            }).build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        binding!!.otpView.setOtpCompletionListener { otp ->
+            val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
+            auth!!.signInWithCredential(credential)
+                .addOnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        val intent = Intent(this@OTPActivity,SetupProfileActivity::class.java)
+                        startActivity(intent)
+                        finishAffinity()
+                    }else{
+                        Toast.makeText(this@OTPActivity,"Operação falhou!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
---
+++ app/src/main/java/com/example/aobachat/SetupProfileActivity.kt
@@ -0,0 +1,121 @@
+package com.example.aobachat
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import com.example.aobachat.databinding.ActivitySetupProfileBinding
+import com.example.aobachat.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+
+class SetupProfileActivity : AppCompatActivity() {
+
+    var binding : ActivitySetupProfileBinding? = null
+    var auth : FirebaseAuth? = null
+    var database : FirebaseDatabase? = null
+    var storage : FirebaseStorage? = null
+    var selectedImage : Uri? = null
+    var dialog : ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog!!.setMessage("Atualizando Perfil...")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        binding!!.imgView.setOnClickListener{
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent,45)
+        }
+
+        binding!!.btnContinue2.setOnClickListener{
+            val name : String = binding!!.editName.text.toString()
+            if(name.isEmpty()){
+                binding!!.editName.setError("Por favor, digite seu nome")
+            }
+            dialog!!.show()
+            if(selectedImage != null){
+                val reference = storage!!.reference.child("Profile")
+                    .child(auth!!.uid!!)
+                reference.putFile(selectedImage!!).addOnCompleteListener{ task ->
+                    if (task.isSuccessful){
+                        reference.downloadUrl.addOnCompleteListener{ uri ->
+                            val imageUrl = uri.toString()
+                            val uid = auth!!.uid
+                            val phone = auth!!.currentUser!!.phoneNumber
+                            val name : String = binding!!.editName.text.toString()
+                            val user = User(uid,name,phone,imageUrl)
+                            database!!.reference
+                                .child("users")
+                                .child(uid!!)
+                                .setValue(user)
+                                .addOnCompleteListener{
+                                    dialog!!.dismiss()
+                                    val intent = Intent(this@SetupProfileActivity,MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                        }
+                    }else{
+                        val uid = auth!!.uid
+                        val phone = auth!!.currentUser!!.phoneNumber
+                        val name : String = binding!!.editName.text.toString()
+                        val user = User(uid,name,phone,"Sem imagem")
+                        database!!.reference
+                            .child("users")
+                            .child(uid!!)
+                            .setValue(user)
+                            .addOnCanceledListener {
+                                dialog!!.dismiss()
+                                val intent = Intent(this@SetupProfileActivity,MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                    }
+
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(data !=  null){
+            if(data.data != null){
+                val uri = data.data //filePath
+                val storage = FirebaseStorage.getInstance()
+                val time = Date().time
+                val reference = storage.reference
+                    .child("Profile")
+                    .child(time.toString() + "")
+                reference.putFile(uri!!).addOnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        reference.downloadUrl.addOnCompleteListener{ uri ->
+                            val filePath = uri.toString()
+                            val obj = HashMap<String,Any>()
+                            obj["image"]=filePath
+                            database!!.reference
+                                .child("users")
+                                .child(FirebaseAuth.getInstance().uid!!)
+                                .updateChildren(obj).addOnSuccessListener {  }
+                        }
+                    }
+                }
+                binding!!.imgView.setImageURI(data.data)
+                selectedImage = data.data
+            }
+        }
+    }
+}
